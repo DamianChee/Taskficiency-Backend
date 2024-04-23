01cfbe9bf656af45d5fd3211a288b9a58e727fa6
Damian Chee <silverousmechs@gmail.com>
2024-04-24 01:43:02 +0800

    Create Team.js

---
+++ src/models/Team.js
@@ -0,0 +1,34 @@
+// models/Teams.js
+
+module.exports = (sequelize, DataTypes) => {
+  const Teams = sequelize.define("Teams", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Users", // This is a reference to another model
+        key: "id",
+      },
+    },
+  });
+
+  return Teams;
+};
